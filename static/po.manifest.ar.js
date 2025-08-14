e9d365d69ecf7ff72654628cdf30997b2070ac2c
rachitrachu <rachit.rachu@gmail.com>
2025-08-14 12:46:59 +0530

    upload new installed files

---
+++ static/po.manifest.ar.js
@@ -0,0 +1,27 @@
+window.cockpit_po = {
+ "": {
+  "plural-forms": (n) => n==0 ? 0 : n==1 ? 1 : n==2 ? 2 : n%100>=3 && n%100<=10 ? 3 : n%100>=11 ? 4 : 5,
+  "language": "ar",
+  "language-direction": "rtl"
+ },
+ "Diagnostic reports": [
+  null,
+  "تقارير التشخيص"
+ ],
+ "Kernel dump": [
+  null,
+  "عطب نواة النظام"
+ ],
+ "Networking": [
+  null,
+  "التشبيك"
+ ],
+ "SELinux": [
+  null,
+  "SELinux"
+ ],
+ "Storage": [
+  null,
+  "التخزين"
+ ]
+};
