b831ef3889fdab043e4e6b66de8919155e567ee9
rachitrachu <rachit.rachu@gmail.com>
2025-08-25 15:39:02 +0530

    1

--- xos-networking/netplan_manager.py
+++ xos-networking/netplan_manager.py
@@ -4,6 +4,7 @@ import yaml
 import os
 import subprocess
 import json
+import traceback
 
 NETPLAN_DIR = '/etc/netplan'
 
@@ -18,63 +19,75 @@ def load_netplan():
 
 # Helper to write a netplan YAML file
 def write_netplan(fname, data):
-    with open(os.path.join(NETPLAN_DIR, fname), 'w') as f:
-        yaml.safe_dump(data, f, default_flow_style=False)
+    try:
+        with open(os.path.join(NETPLAN_DIR, fname), 'w') as f:
+            yaml.safe_dump(data, f, default_flow_style=False)
+    except Exception as e:
+        print(json.dumps({'error': f'Failed to write netplan file: {e}', 'trace': traceback.format_exc()}))
+        sys.exit(1)
 
 # Apply netplan changes
 def apply_netplan():
-    subprocess.run(['netplan', 'apply'], check=True)
+    try:
+        subprocess.run(['netplan', 'apply'], check=True)
+    except Exception as e:
+        print(json.dumps({'error': f'Failed to apply netplan: {e}', 'trace': traceback.format_exc()}))
+        sys.exit(1)
 
 # Main entry point
 def main():
-    req = json.load(sys.stdin)
-    action = req.get('action')
-    config = req.get('config')
-    fname = req.get('filename', '99-cockpit.yaml')
+    try:
+        req = json.load(sys.stdin)
+        action = req.get('action')
+        config = req.get('config')
+        fname = req.get('filename', '99-cockpit.yaml')
 
-    # Load or create config file
-    configs = load_netplan()
-    netplan = configs.get(fname, {'network': {}})
-    network = netplan['network']
+        # Load or create config file
+        configs = load_netplan()
+        netplan = configs.get(fname, {'network': {}})
+        network = netplan['network']
 
-    if action == 'add_bond':
-        # config: {name, mode, interfaces}
-        if 'bonds' not in network:
-            network['bonds'] = {}
-        network['bonds'][config['name']] = {
-            'interfaces': config['interfaces'],
-            'parameters': {'mode': config['mode']},
-            'dhcp4': True
-        }
-    elif action == 'add_vlan':
-        # config: {name, id, link}
-        if 'vlans' not in network:
-            network['vlans'] = {}
-        network['vlans'][config['name']] = {
-            'id': config['id'],
-            'link': config['link'],
-            'dhcp4': True
-        }
-    elif action == 'add_bridge':
-        # config: {name, interfaces}
-        if 'bridges' not in network:
-            network['bridges'] = {}
-        network['bridges'][config['name']] = {
-            'interfaces': config['interfaces'],
-            'dhcp4': True
-        }
-    elif action == 'delete':
-        # config: {type, name}
-        if config['type'] in network and config['name'] in network[config['type']]:
-            del network[config['type']][config['name']]
-    else:
-        print(json.dumps({'error': 'Unknown action'}))
+        if action == 'add_bond':
+            # config: {name, mode, interfaces}
+            if 'bonds' not in network:
+                network['bonds'] = {}
+            network['bonds'][config['name']] = {
+                'interfaces': config['interfaces'],
+                'parameters': {'mode': config['mode']},
+                'dhcp4': True
+            }
+        elif action == 'add_vlan':
+            # config: {name, id, link}
+            if 'vlans' not in network:
+                network['vlans'] = {}
+            network['vlans'][config['name']] = {
+                'id': config['id'],
+                'link': config['link'],
+                'dhcp4': True
+            }
+        elif action == 'add_bridge':
+            # config: {name, interfaces}
+            if 'bridges' not in network:
+                network['bridges'] = {}
+            network['bridges'][config['name']] = {
+                'interfaces': config['interfaces'],
+                'dhcp4': True
+            }
+        elif action == 'delete':
+            # config: {type, name}
+            if config['type'] in network and config['name'] in network[config['type']]:
+                del network[config['type']][config['name']]
+        else:
+            print(json.dumps({'error': 'Unknown action'}))
+            sys.exit(1)
+
+        # Save and apply
+        write_netplan(fname, netplan)
+        apply_netplan()
+        print(json.dumps({'result': 'success'}))
+    except Exception as e:
+        print(json.dumps({'error': str(e), 'trace': traceback.format_exc()}))
         sys.exit(1)
-
-    # Save and apply
-    write_netplan(fname, netplan)
-    apply_netplan()
-    print(json.dumps({'result': 'success'}))
 
 if __name__ == '__main__':
     main()
