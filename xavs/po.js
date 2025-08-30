3ef7de767815b98653ba09dd048817bdf2f499ee
Ankit Gupta <153551386+aguptax@users.noreply.github.com>
2025-08-30 18:07:45 +0530

    css module

---
+++ xavs/po.js
@@ -0,0 +1,8 @@
+/* XAVS Overview translations - placeholder */
+window.cockpit_po = {
+    "": {
+        "plural-forms": (n) => n !== 1,
+        "language": "en",
+        "language-direction": "ltr"
+    }
+};
